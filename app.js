061a32ff85a13c6129f36f77796aadfe43801dfa
Ryne Emory <ryneemory@gmail.com>
2015-08-25 21:52:52 -0600

    colors

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var app = angular.module('mouseOverDirectiveApp', [])
 
+
 app.directive('gsChangeBackground', function() {
   return {
     link: function(scope, element, attrs) {
-
+      // console.log(element);
       var oldColor = element.css('background-color');
-
       element.on('mouseenter', function(event) {
-        element.css('background-color', 'yellow');
+        element.css('background-color', 'scope.color');
       });
 
       element.on('mouseleave', function(event) {
